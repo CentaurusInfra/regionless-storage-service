9f47637868738770c00fb48d990928f1ec11af4a
Peng Du <rick.peng.du@gmail.com>
2022-07-29 19:23:43 +0000

    fix

--- scripts/test_infra/log_into_rkv.sh
+++ scripts/test_infra/log_into_rkv.sh
@@ -8,5 +8,5 @@ else
     exit 1
 fi
 
-ycsb_ip=`aws ec2 describe-instances --region us-west-2 --filters "Name=tag-value,Values=pengdu-rkv-lab-rkv" "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].PublicIpAddress' --output=text`
-ssh -i regionless_kv_service_key_us_west_2.pem ubuntu@$ycsb_ip
+rkv_ip=`aws ec2 describe-instances --region us-west-2 --filters "Name=tag-value,Values=$prefix-rkv-lab-rkv" "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].PublicIpAddress' --output=text`
+ssh -i regionless_kv_service_key_us_west_2.pem ubuntu@$rkv_ip
